dadd8e0b4025c457f3a09458a6b88f85a646fae6
soniamdn <44521635+soniamdn@users.noreply.github.com>
2018-11-14 12:05:20 +0100

    add

--- test.js
+++ test.js
@@ -1,25 +1,5 @@
-<script> 
-<!--CODE FAIT PAR PYROFLO HTTP://WWW.ABCPYRO.FR.ST-->
-// temps qui s'écoule après que la page soit chargée noté x
-// temps qui s'écoule après que la fenêtre se soit ouverte après x secondes noté y
-
- var window_handle;  // variable globale du script, à ne surtout pas modifier !!!
- function open_window() // on défini la fonction qui va ouvrir la fenêtre
- {
-window_handle = window.open("http://www.abcpyro.fr.st"); // ici c'est l'URL complète de la page à ouvrir attention de ne pas supprimer les guillemets
- } // on ferme la fonction open_window()
- 
- function close_window() // on défini la fonction qui va fermer la fenêtre qu'on a ouvert précédemment
- {
- window_handle.close(); // on ferme la page comme window.close() à part que dans notre cas, on fermer une variable donc window_nomdelavariable.close()
- } // on ferme la fonction close_window()
-
-</script>
-
-<script>
-
-window.setInterval("open_window();", 10000, "JavaScript"); // 10000 est le temps x en MILISECONDE
-window.setInterval(" window_handle.close();", 20000, "JavaScript"); // 20000 est le temps y EN MILISECONDE
-
-</script>
-<!--Fin du JavaScript permettant l'ouverture et la fermeture automatique après 2 temps choisis-->
+if (confirm("Voulez-vous entrer dans le site?")) {
+  txt = "Vous avez appuyé sur OK!";
+} else {
+  txt = "Vous ne voulez pas voir le site!";
+}
