9b7b2bb0378aad7ea260f656a85b4f9cb37b8538
soniamdn <44521635+soniamdn@users.noreply.github.com>
2018-11-15 11:51:18 +0100

    add

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-setTimeout("alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!');", 10000);
+setTimeout("var rep = confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!');", 10000);
+
+if (rep ==true) 
+
+{
+// url d'envoi si Ok est cliqué
+self.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+else{
+// url d'envoi si Annulé est cliqué
+self.location.href="http://";
+}
