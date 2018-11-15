c22b052593d8816eba8551842b372b607fa0b302
soniamdn <soniamadoni@hotmail.fr>
2018-11-15 22:55:22 +0100

    add

--- test.js
+++ test.js
@@ -1,12 +1,7 @@
-setTimeout("var rep = confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!');", 10000);
+$(function(){
+$("#info").hide();
+setTimeout(function () {
+   $('#info').trigger('click');
+}, 10000);
 
-if (rep ==true) 
-
-{
-// url d'envoi si Ok est cliqué
-self.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-}
-else{
-// url d'envoi si Annulé est cliqué
-self.location.href="http://";
-}
+});
