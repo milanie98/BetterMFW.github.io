9d9c643a0da27a7b0cc0da9fe3b03f14c69e5db5
soniamdn <44521635+soniamdn@users.noreply.github.com>
2018-11-15 23:32:38 +0100

    add

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 setTimeout("var rep = confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!');", 10000);
 
-if (rep ==true) 
-
-{
+if (rep ==true) {
 
 self.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
+
 else{
 
 self.location.href="http://";
