13903eaeb9c27aa9426d4e4538acf2728b4a1423
soniamdn <soniamadoni@hotmail.fr>
2018-11-15 23:39:44 +0100

    add

---
+++ README.md
@@ -0,0 +1,11 @@
+# BetterMotherFuckerWebsite
+
+> El Karch Chaïma et Madoni Sonia
+
+Voilà la reproduction du site comme demandé. Les codes sont dans les fichiers ci-joint avec l'aide du format ```.css``` , ```.html``` et ```.js```
+
+On peut voir une fenêtre popup s'ouvrir au bout de 10secondes comme demandé.
+
+[BetterMotherFuckerWebsite](https://soniamdn.github.io/BetterMFW.github.io/) 
+
+> Fucking good reading :cool:
--- test.js
+++ test.js
@@ -1,7 +1,24 @@
-$(function(){
-$("#info").hide();
-setTimeout(function () {
-   $('#info').trigger('click');
-}, 10000);
+setTimeout("var rep = confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!');", 10000);
 
-});
+<<<<<<< HEAD
+if (rep ==true) 
+
+{
+// url d'envoi si Ok est cliqué
+self.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+else{
+// url d'envoi si Annulé est cliqué
+self.location.href="http://";
+}
+=======
+if (rep ==true) {
+
+self.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+
+else{
+
+self.location.href="http://";
+}
+>>>>>>> 9d9c643a0da27a7b0cc0da9fe3b03f14c69e5db5
