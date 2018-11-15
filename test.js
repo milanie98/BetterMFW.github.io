fbe8b744d1d9300d63f895587e01254b141e9d51
soniamdn <44521635+soniamdn@users.noreply.github.com>
2018-11-15 23:28:40 +0100

    add

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
-setTimeout("alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!');", 10000);
-if (confirm('Voulez vous entrer sur le site?'));
-	else ('ok');
+setTimeout("var rep = confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!');", 10000);
+
+if (rep ==true) 
+
+{
+
+self.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+else{
+
+self.location.href="http://";
+}
