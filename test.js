c770f5ec4a03d6ecb869628df1037096a0f48f23
soniamdn <44521635+soniamdn@users.noreply.github.com>
2018-11-16 12:28:41 +0100

    add

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
 setTimeout("var rep = confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!');", 10000);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 if (rep ==true) 
 
 {
@@ -12,20 +10,3 @@ else{
 // url d'envoi si Annulé est cliqué
 self.location.href="http://";
 }
-=======
-=======
->>>>>>> 9d9c643a0da27a7b0cc0da9fe3b03f14c69e5db5
-if (rep ==true) {
-
-self.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-}
-
-else{
-
-self.location.href="http://";
-}
-<<<<<<< HEAD
->>>>>>> 9d9c643a0da27a7b0cc0da9fe3b03f14c69e5db5
-=======
->>>>>>> 9d9c643a0da27a7b0cc0da9fe3b03f14c69e5db5
- 
