70091301291aa72c13870d4d862a99b26039a35f
soniamdn <44521635+soniamdn@users.noreply.github.com>
2018-11-16 12:30:18 +0100

    add

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
 setTimeout("var rep = confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!');", 10000);
 
-if (rep ==true) 
+if (rep ==true) {
 
-{
-// url d'envoi si Ok est cliqué
 self.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
+
 else{
-// url d'envoi si Annulé est cliqué
+
 self.location.href="http://";
 }
